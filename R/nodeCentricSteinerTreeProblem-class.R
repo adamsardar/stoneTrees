b32d6769a5ed396a6b4b9260cea5e3a410b3caa6
Adam Sardar <adam@sardar.me.uk>
2019-05-13 09:58:19 +0100

    Initial work on a OOP version of Steiner solver

--- R/nodeCentricSteinerTreeProblem-class.R
+++ R/nodeCentricSteinerTreeProblem-class.R
@@ -3,25 +3,28 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
 
                               public = list(
 
-
-
-
                                 initialize = function(){
 
-                                  #TODO parameter checking
+                                  # TODO parameter checking
 
+                                  # search graph - undirected and simple. NAMED
+
+                                  # edgeDT - both directions
+                                  # nodeDT with node indicies
+
+                                  # nodeScores - all -1 if absent
 
                                   private$addFixedTerminalConstraints()
-
+                                  private$addNodeDegreeInequalities()
+                                  private$addTwoCycleInequalities()
                                 },
-
-
 
                                 solve = function(){}
 
-                                getFixedTerminalConstraints <- function(){private$fixedTerminalConstraints}
-
-
+                                #Access to the constaint matricies will make for easier testing
+                                getFixedTerminalConstraints <- function(){private$fixedTerminalConstraints},
+                                getNodeDegreeConstraints <- function(){private$nodeDegreeConstraints},
+                                getTwoCycleConstraints <- function(){private$twoCycleConstraints}
                               ),
 
                               private = list(
@@ -45,10 +48,41 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
                                 },
 
                                 # Constraint 5.) Only potential or fixed terminals can have a degree of 1, all other nodes must have degree >= 2
-                                addNodeDegreeInequalities = function(){},
+                                addNodeDegreeInequalities = function(){
 
-                                # Constraint 6.) If a potential node i sits adjacent to a potential terminal j, then j must be included if i is
-                                addTwoCycleInequalities = function(){},
+                                  nodeDegreeInequalities_variables <- get.adjacency(private$searchGraph, sparse = TRUE)
+                                  diag(nodeDegreeInequalities_variables) <- -2
+                                  diag(nodeDegreeInequalities_variables)[ private$terminalIndicies ] <- -1
+
+                                  private$nodeDegreeConstraints <- list(
+                                    variables = nodeDegreeInequalities_variables,
+                                    directions = rep(">=", nrow(nodeDegreeInequalities_variables)),
+                                    rhs = rep(0, nrow(nodeDegreeInequalities_variables)) )
+
+                                  invisible(self)
+                                },
+
+                                # Constraint 6.) If a potential node j sits adjacent to a potential terminal i, then i must be included if j is
+                                addTwoCycleInequalities = function(){
+
+                                  # Two cycle constraints are simple - for each edge i->j around a potential terminal i, have a setup which enforces its inclusion if j is present in solutoion
+                                  twoCycle_variables <- private$edgeDT[fromNodeID %in% private$potentialTerminalIndicies,
+                                                               sparseMatrix(i = c(.edgeID, .edgeID) ,
+                                                                            j = c(fromNodeID, toNodeID),
+                                                                            x = rep(c(1, -1) , each = length(.edgeID) ),
+                                                                            dims = c( max(.edgeID), vcount(private$searchGraph)),
+                                                                            dimnames = list( paste("twoCycleOnEdge", 1:max(.edgeID)) , V(private$searchGraph)$name))]
+
+                                  # We only care about edges *from* a potential terminal node
+                                  twoCycle_variables  %<>% .[ private$edgeDT[fromNodeID %in% private$potentialTerminalIndicies, .edgeID], ]
+
+                                  private$twoCycleConstraints <- list(
+                                    variables = twoCycle_variables,
+                                    directions = rep(">=", nrow(twoCycle_variables)),
+                                    rhs = rep(0, nrow(twoCycle_variables)) )
+
+                                  invisible(self)
+                                },
 
                                 # Constraint 2.) Aims to endorce connections (via minimal node seperators) for pairs of terminals that are disconnected
                                 # Applied lazily over terminal pairs in seperate components
@@ -63,6 +97,11 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
 
                                 nodeScores = numeric(),
 
-                                fixedTerminalConstraints = list()
+                                fixedTerminalConstraints = list(),
+                                nodeDegreeConstraints = list(),
+                                twoCycleConstraints = list(),
+
+                                edgeDT = data.table(),
+                                nodeDT = data.table()
                               )
 )
