34f68ef4e18225393bc3912a923e6cb893fc5c2f
Adam Sardar <adam@sardar.me.uk>
2019-05-13 14:23:57 +0100

    Added connectivity constraints. Constraint 2.) from Fischetti et al.

--- R/nodeCentricSteinerTreeProblem-class.R
+++ R/nodeCentricSteinerTreeProblem-class.R
@@ -3,28 +3,43 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
 
                               public = list(
 
-                                initialize = function(){
+                                initialize = function(network, solverChoice = "GLPK", verbose = TRUE){
 
-                                  # TODO parameter checking
-
+                                  # TODO check network
                                   # search graph - undirected and simple. NAMED
 
                                   # edgeDT - both directions
                                   # nodeDT with node indicies
-
-                                  # nodeScores - all -1 if absent
+                                  ## nodeScores - all -1 if absent
 
                                   private$addFixedTerminalConstraints()
                                   private$addNodeDegreeInequalities()
                                   private$addTwoCycleInequalities()
+
+                                  # populate initial connectivity constraint matrix - initially empty as we only add these constraints 'lazily' (i.e. as needed to repair disconnected solutions)
+                                  private$twoCycleConstraints <- list( variables = Matrix(0, sparse = TRUE, nrow = 0,
+                                                                                          ncol = vcount(private$searchGraph),
+                                                                                          dimnames = list(NULL, V(private$searchGraph)$name)),
+                                                                        directions = character(),
+                                                                        rhs = numeric() )
+
+                                  # TODO validate solver
+                                  private$solver <- solverChoice
+
+                                  #TODO validate verbose
+                                  private$verbosity <- verbose
                                 },
 
-                                solve = function(){}
+                                solve = function(){stop("Not yet implemented")},
+
+                                getNodeDT = function(){ private$nodeDT },
+                                getEdgeDT = function(){ private$edgeDT },
 
                                 #Access to the constaint matricies will make for easier testing
-                                getFixedTerminalConstraints <- function(){private$fixedTerminalConstraints},
-                                getNodeDegreeConstraints <- function(){private$nodeDegreeConstraints},
-                                getTwoCycleConstraints <- function(){private$twoCycleConstraints}
+                                getFixedTerminalConstraints = function(){ private$fixedTerminalConstraints },
+                                getNodeDegreeConstraints = function(){ private$nodeDegreeConstraints },
+                                getTwoCycleConstraints = function(){ private$twoCycleConstraints },
+                                getConnectivityConstraints = function(){ private$connectivityConstraints }
                               ),
 
                               private = list(
@@ -65,6 +80,10 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
                                 # Constraint 6.) If a potential node j sits adjacent to a potential terminal i, then i must be included if j is
                                 addTwoCycleInequalities = function(){
 
+                                  # 2-cycle inequalities (6)
+                                  # Observe the following: if node i ∈ V is adjacent to a node j ∈ T_p, so that c_ij< p_j, then if i is part of the optimal solution, j has to be included as well
+                                  # y_i ≤ y_j ∀ i ∈ V, j ∈ T_p
+
                                   # Two cycle constraints are simple - for each edge i->j around a potential terminal i, have a setup which enforces its inclusion if j is present in solutoion
                                   twoCycle_variables <- private$edgeDT[fromNodeID %in% private$potentialTerminalIndicies,
                                                                sparseMatrix(i = c(.edgeID, .edgeID) ,
@@ -85,8 +104,94 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
                                 },
 
                                 # Constraint 2.) Aims to endorce connections (via minimal node seperators) for pairs of terminals that are disconnected
-                                # Applied lazily over terminal pairs in seperate components
-                                addConnectivityConstraints = function(){}
+                                # This is the only really complicated constraint set - study of it should be in conjunction with the paper Fischetti et al.
+                                # Only applied over terminal pairs in seperate components
+                                addConnectivityConstraints = function(){
+
+                                  # Using a dedicated column in private$nodeDT for cluster membership
+                                  if(private$nodeDT[,all(is.na(inComponent))]){ stop("No nodes in components for solution!") }
+
+                                  # Break solution into connected components. If only a single component, then we're done
+                                  componentsSummaryDT <- private$nodeDT[!is.na(inComponent), .N, by = inComponent]
+                                  terminalsInComponentsDT <-  private$nodeDT[!is.na(inComponent) & isTerminal]
+
+                                  solutionIsConnected <- nrow(componentsSummaryDT) == 1
+
+                                  if(private$verbosity){
+
+                                    message(ifelse(solutionIsConnected, "##Solution is connected!##", "##Solution is disconnected!##"))
+                                    message("With ", componentsSummaryDT$N," nodes and ", nrow(terminalsInComponentsDT)," terminals.")
+                                  }
+
+                                  if(solutionIsConnected){ invisible(self) }
+
+                                  # Create terminal pairs
+                                  # Filter for terminal pairs in different components
+                                  allTerminalPairs <- combn( names(private$terminalIndicies), 2) %>% t %>% as.data.table
+                                  setnames(allTerminalPairs, c("T1nodeID","T2nodeID")) # Note that we are working by node indicies here to avoid using node names
+
+                                  allTerminalPairs[private$nodeDT, c("T1inComponent","T1name") := list(inComponent, name), on = .(T1nodeID = .nodeID)]
+                                  allTerminalPairs[private$nodeDT, c("T2inComponent","T2name") := list(inComponent, name), on = .(T2nodeID = .nodeID)]
+
+                                  # Only need keep node pairs where both nodes are in the solution but not in the same component
+                                  terminalPairsInDifferentComponents <-
+                                       allTerminalPairs[!is.na(T1inComponent) & !is.na(T1inComponent)][T1inComponent != T2inComponent]
+
+                                  # Pre-compute the component surfaces (A(C_i) in paper) - i.e. nodes that are adjacent to C_i but not in C_i
+                                  clusterSurfacesDT <- private$nodeDT[!is.na(inComponent)] %>%
+                                    merge(private$edgeDT, by.x = ".nodeID", by.y = "fromNodeID") %>%
+                                    .[,.(componentSurfaceNodeID = .SD[!toNodeID %in% .nodeID, unique(toNodeID)]), by = inComponent]
+
+                                  # Matrix to store connectivity constraints
+                                  allConnectivityConstraints <- Matrix(0, sparse = TRUE,
+                                                                       nrow = nrow(terminalPairsInDifferentComponents),
+                                                                       ncol = vcount(private$searchGraph),
+                                                                       dimnames = list(NULL, V(private$searchGraph)$name) )
+
+                                  graphDiameter <- diameter(private$searchGraph)
+
+                                  if(private$verbosity) message("Adding ",nrow(terminalPairsInDifferentComponents)," connectivity constraint based upon node seperators")
+
+                                  # For each terminal pair i,j; compute R_j (reachable set from R_j in graph ommiting C_i) and compute the minimum node-seperator set N_ij = A(C_i) intersect R_j
+                                  for(r in 1:nrow(terminalPairsInDifferentComponents)){
+
+                                    terminalPair <- terminalPairsInDifferentComponents[r,]
+
+                                    C_i_componentSurfaceNodeIDs <- clusterSurfacesDT[ inComponent ==  terminalPair$T1inComponent, componentSurfaceNodeID]
+
+                                    graph_omitCi <- delete_vertices(fullSearchGraph,
+                                                                    V(fullSearchGraph)[ private$nodeDT[inComponent ==  terminalPair$T1inComponent, .nodeID] ])
+
+                                    # Note that this is the graph LESS the Ci componenet - hence we can't precompute upfront.
+                                    # Notice the use of terminal name, not ID
+                                    # Reffered to as Rj in the paper
+                                    nodeNamesReachableFromJ <- V(make_ego_graph(graph_omitCi,
+                                                                                nodes = V(graph_omitCi)[name == terminalPair$T2name],
+                                                                                order = graphDiameter)[[1]])$name
+
+                                    # Nodes adjacent to C_i and in R_j consititues a minimal node seperator 𝒩(i,j)
+                                    # Identify minimal node seperator set 𝒩(i,j) = A(C_i) intersect R_j
+                                    minNodeSep_ij_nodeIDs <- intersect(C_i_componentSurfaceNodeIDs,
+                                                                       private$nodeDT[name %in% nodeNamesReachableFromJ, .nodeID])
+
+                                    # Add connectivity constraint
+                                    # y(N) ≥ y_i + y_j -1 ∀ i,j ∈ T, i ≠ j  ∀N ∈ 𝒩(i,j)
+                                    # i.e. if you have i then you must have a node seperator to have j included in the result
+                                    # This acts as a way of repairing a disconnected solution in a lazy fashion (there are exponentially many node seperators, hence enumerating them all up front is not feasible)
+                                    allConnectivityConstraints[r, terminalPair$T1nodeID] <- -1
+                                    allConnectivityConstraints[r, terminalPair$T2nodeID] <- -1
+
+                                    allConnectivityConstraints[r, minNodeSep_ij_nodeIDs] <- 1
+                                  }
+
+                                  # Append connectivity constraints matrix to existing
+                                  private$twoCycleConstraints <- list( variables = rbind(private$twoCycleConstraints$variables, allConnectivityConstraints) )
+
+                                  private$twoCycleConstraints$directions <- rep(">=", nrow(private$twoCycleConstraints$variables) )
+                                  private$twoCycleConstraints$rhs <- rep(-1, nrow(private$twoCycleConstraints$variables) )
+
+                                  invisible(self)
+                                },
 
                                 searchGraph = graph.empty(),
 
@@ -101,7 +206,12 @@ nodeCentricSteinerTreeProblem <- R6Class("nodeCentricSteinerTreeProblem",
                                 nodeDegreeConstraints = list(),
                                 twoCycleConstraints = list(),
 
+                                connectivityConstraints = list(),
+
                                 edgeDT = data.table(),
-                                nodeDT = data.table()
+                                nodeDT = data.table(),
+
+                                solver = character(),
+                                verbosity = logical()
                               )
 )
