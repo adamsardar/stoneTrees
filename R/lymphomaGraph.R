428e9334bd69c0310b902f545e17d2b403f90534
Adam Sardar <adam@sardar.me.uk>
2019-05-16 12:06:51 +0100

    Greater provinance for lymphoma graph

--- R/lymphomaGraph.R
+++ R/lymphomaGraph.R
@@ -1,6 +1,7 @@
-#' Lymphoma example from SteinLib
-#' 
+#' Lymphoma example from DIMAC11 challenge
+#'
 #' An udirected graph representing a Maximum-Weight Connected Subgraph (MWCS) problem from a genetic context
+#' Instance used in the DIMACS11 Steiner Tree problems challengs (2015).
 #'
 #' @docType data
 #' @usage data(lymphomaGraph)
@@ -10,5 +11,5 @@
 #'   \item{name}{Unique identifiers for each vertex - note, not an index despite being character numbers}
 #'   \item{nodeScore}{A score or weight on the node. Produced using the BioNet algroithm using diffuse large B-cell lymphoma data}
 #' }
-#' @source \url{http://bioconductor.org/packages/release/data/experiment/html/DLBCL.html}
+#' @source \url{http://dimacs11.zib.de/downloads.html}
 "lymphomaGraph"
