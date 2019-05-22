bf688027a67b621d76fe5ccb3cd4596859c0c72b
Adam Sardar <adam@sardar.me.uk>
2019-05-22 12:30:26 +0100

    Documented use of the Steiner Forest class. Work towards #4

--- R/nodeCentricSteinerForestProblem-class.R
+++ R/nodeCentricSteinerForestProblem-class.R
@@ -1,6 +1,56 @@
-
-
+#' Solve multiple bootstrap Minimum Steiner Tree problems (aka Steiner Forest procedure)
+#'
+#' Given a set of seeds/fixed terminals a Minimum Steiner Tree can be found. One might well be interested in studying the common nodes that
+#' would be included with, say, just 50% of the seed set. This process is known as a 'bootstrap' in statistics and this class looks to
+#' repeatedly sample seeds to produce a consensus set of MStTP solutions. Sub-solutions can also be collected, albeit at an increased burden on the
+#' solver (and therefore dramatically increasing the time).
+#'
+#' This class is derived from *subOptimalSteinerProblem* and in turn *nodeCentricSteinerTreeProblem*: all methods available in the superclass are available here. The difference is that after
+#' each acceptable solution is found, the solution is a.) stored in a bootstrap solution pool and b.) used to generate a 'novelty' constraint on future solutions. For each bootstrap run, the solution pool
+#' is flushed and the process re-rerun. In the end, all of the boostrap solutions are in the bootstrap solution pool.
+#'
+#' @docType class
+#' @format R6Class \code{nodeCentricSteinerForestProblem} Construct an object representation of the bootstraped Steiner Tree process (aka Steiner Forest routine)
+#'
+#' @section methods:
+#' Alongisde those for *nodeCentricSteinerTreeProblem* and *subOptimalSteinerProblem*
+#' \describe{
+#'    \item{\code{new(network, solverChoice = chooseSolver(), verbose = TRUE, solverTimeLimit = 300, solverTrace = as.integer(verbose), solutionTolerance = 0)}}{Constructor for the nodeCentricSteinerForestProblem class. Note the loss of 'presolveGraph'; the repeated resampling of fixed terminal nodes prevents this.}
+#'    \item{\code{sampleMultipleBootstrapSteinerSolutions(nBootstraps = 5, maxItr = 0, resamplingProbability= 0.5)}}{Run the bootstrap procedure nBootstraps times, each time resampling seeds with pSuccess = resamplingProbability, collecting degenerate or suboptimal solutions for maxItr times.}
+#'    \item{\code{getBootstrapSolutionPoolGraphs(collapseSols = TRUE)}}{Either return a list of solutions within tolerence (collapseSols = FALSE) or pool all solutions together and return a single graph (collapseSols = TRUE, defaults)}
+#'    \item{\code{...}{other methods are of minimal interest to a general user} }
+#' ' }
+#'
+#' @examples
+#' library(igraph)
+#'
+#' #Prepare a simple seed-based Steiner sampling in a reasonable sized network
+#'
+#' fixedTerminalLymphomaGraph <- lymphomaGraph
+#' V(fixedTerminalLymphomaGraph)$isTerminal <- FALSE
+#' V(fixedTerminalLymphomaGraph)[nodeScore > 0]$isTerminal <- TRUE
+#' fixedTerminalLymphomaGraph <- delete_vertex_attr(fixedTerminalLymphomaGraph, "nodeScore")
+#'
+#'
+#' # Example of solving *just* the single-solution Minimum Steiner Tree Problem
+#' MStTPsingle <- nodeCentricSteinerTreeProblem$new(fixedTerminalLymphomaGraph)
+#' MStTPsingle$findSingleSteinerSolution()
+#'
+#' #Solve multiple bootstrap Steiner Trees (Steiner Forest)
+#' SteinFor <- nodeCentricSteinerForestProblem$new(fixedTerminalLymphomaGraph)
+#'
+#' #Run two bootstrap routines (resample fixed terminals and solve) and ALSO run the sub-optimal solution searcher thrice
+#' SteinFor$sampleMultipleBootstrapSteinerSolutions(nBootstraps = 2, maxItr = 3)
+#' ## Takes around a minute using RGLPK as the solver
+#'
+#' @references Fischetti M, Leitner M, Ljubić I, Luipersbeck M, Monaci M, Resch M, et al. Thinning out Steiner trees: a node-based model for uniform edge costs. Math Program Comput. dimacs11.cs.princeton.edu; 2017;9: 203–229.
+#' @references Beisser D, Klau GW, Dandekar T, Müller T, Dittrich MT. BioNet: An R-Package for the functional analysis of biological networks. Bioinformatics. 2010;26: 1129–1130.
+#' @references \url{https://en.wikipedia.org/wiki/Steiner_tree_problem}
+#' @family SteinerProblemSolver
+#' @seealso nodeCentricSteinerTreeProblem
+#' @seealso subOptimalSteinerProblem
 #' @importFrom sets set set_union
+#' @export
 nodeCentricSteinerForestProblem <- R6Class("nodeCentricSteinerForestProblem",
                                            inherit = subOptimalSteinerProblem,
                                            public = list(
