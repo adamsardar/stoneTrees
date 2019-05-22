e02690beefbd3eade50070b0b10013266c843d65
Adam Sardar <adam@sardar.me.uk>
2019-05-22 10:39:46 +0100

    Add package description. Work on #4

--- R/stoneTrees-package.R
+++ R/stoneTrees-package.R
@@ -1,15 +1,15 @@
-#' R implementation of Steiner Tree solver
+#' Solve Minimum Steiner Tree (MStT) and Maximum-Weight Connected Subgraph Problems Using Integer Linear Programming
 #'
 #' A package dedicated to finding minimum Steiner trees in networks. Particularly biological ones,
-#' which tend to be very sparse and on the easier end of the spectrum. This package is particularly
-#' interested in Minimum Steiner Tree (MStT), Maximum-Weight Connected Subgraph (MWCS) and
-#' Prize-Collecting Steiner Tree (PCST) problems.
+#' which tend to be very sparse and on the easier end of the spectrum. This package is especially
+#' interested in Minimum Steiner Tree (MStT) and Maximum-Weight Connected Subgraph (MWCS) problems.
 #'
-#' This package serves as a faithful implementation of "Thinning out Steiner Trees".
+#' This package serves as a faithful implementation of "Thinning out Steiner Trees" (with a few bells and whistles added on the sides).
 #'
 #' @docType package
 #' @name stoneTrees
 #' @author Adam Sardar
+#' @references Fischetti M, Leitner M, Ljubić I, Luipersbeck M, Monaci M, Resch M, et al. Thinning out Steiner trees: a node-based model for uniform edge costs. Math Program Comput. dimacs11.cs.princeton.edu; 2017
 #' @import R6
 #' @importFrom data.table ".__T__[<-:base" ".__T__$<-:base" ".__T__[:base"  rbindlist data.table ":=" %like% fread setkey setnames
 #' @import igraph
